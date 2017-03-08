d869d8e14d5d3d19f6f4a99caf56d924e7c93071
Gabriel Sim <gabrielsim1993@gmail.com>
2017-03-08 15:53:24 +0800

    update logic component

---
+++ src/main/java/seedu/task/logic/commands/AddCommand.java
@@ -0,0 +1,66 @@
+package seedu.task.logic.commands;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Phone;
+import seedu.address.model.person.UniquePersonList;
+import seedu.address.model.tag.Tag;
+import seedu.address.model.tag.UniqueTagList;
+
+/**
+ * Adds a task to the task manager.
+ */
+public class AddCommand extends Command {
+
+    public static final String COMMAND_WORD = "add";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a task to the task manager. "
+            + "Parameters: TASK [s/STARTING_DATE] [f/FINISHING_DATE] [#TAG]...\n"
+            + "Example: " + COMMAND_WORD
+            + " submit report from s/Monday by f/Saturday";
+
+    public static final String MESSAGE_SUCCESS = "New task added: %1$s";
+    public static final String MESSAGE_DUPLICATE_TASK = "This task already exists in the task manager";
+
+    private final Task toAdd;
+
+    /**
+     * Creates an AddCommand using raw values.
+     *
+     * @throws IllegalValueException if any of the raw values are invalid
+     */
+    public AddCommand(String title, String description, String startDate, String endDate, Set<String> tags)
+            throws IllegalValueException {
+        final Set<Tag> tagSet = new HashSet<>();
+        for (String tagName : tags) {
+            tagSet.add(new Tag(tagName));
+        }
+        this.toAdd = new Task(
+                new Title(title),
+                new Description(description),
+                new StartDate(startDate),
+                new EndDate(endDate),
+                new UniqueTagList(tagSet)
+        );
+    }
+
+    @Override
+    public CommandResult execute() throws CommandException {
+        assert model != null;
+        try {
+            model.addTask(toAdd);
+            return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd));
+        } catch (UniqueTaskList.DuplicateTaskException e) {
+            throw new CommandException(MESSAGE_DUPLICATE_TASK);
+        }
+
+    }
+
+}
